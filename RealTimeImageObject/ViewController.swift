173918cde67daf89b3481ae2798edb6f0bb35586
yuta316 <iy.24.bump.09@gmail.com>
2021-01-25 19:23:03 +0900

    Initial Commit

---
+++ RealTimeImageObject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeImageObject
+//
+//  Created by 石川裕太 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
